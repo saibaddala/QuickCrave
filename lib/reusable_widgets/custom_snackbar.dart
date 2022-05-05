fa4af2997b22a59fdb8979844cf674b26f399459
Baddala Sai Kumar Reddy <saibaddala172@gmail.com>
2022-05-05 10:01:40 +0530

    about to begin authentication

---
+++ lib/reusable_widgets/custom_snackbar.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:foodart/reusable_widgets/big_text.dart';
+import 'package:get/get.dart';
+
+void showCustomSnackBar(
+    {required String message, bool isError = true, String title = "Error"}) {
+  Get.snackbar(title, message,
+      titleText: BigText(
+        text: title,
+        textColor: Colors.white,
+      ),
+      messageText: Text(
+        message,
+        style:const TextStyle(color: Colors.white),
+      ),
+      backgroundColor: Colors.redAccent,
+      colorText: Colors.white,
+      snackPosition: SnackPosition.TOP);
+}
